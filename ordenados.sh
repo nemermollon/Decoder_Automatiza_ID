d47061cef80634803f35956505bb0f48fc6b46f5
Nemer <nemeralbano@hotmail.com>
2023-01-16 11:57:36 -0300

    Delete ordenados.sh

--- ordenados.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcc extrai_dados_automatiza.c -o extrai_dados_automatiza
-./extrai_dados_automatiza $(ls *.dat | grep 'user' | sort -r | head -n1) > users
-./extrai_dados_automatiza $(ls *.dat | grep 'user' | sort -r | head -n1) | cut -d ":" -f3 | tr -d ' ' > ids
-paste ids users | sort
